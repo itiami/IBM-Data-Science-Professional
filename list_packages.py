81e49625380af630aa348b48eb9b431007edfd4c
itiami <info@wali.co>
2025-06-29 14:18:01 +0200

    C10 and Earlier

--- list_packages.py
+++
@@ -1,6 +0,0 @@
-import pkg_resources
-import site
-
-# Get all installed packages
-for dist in pkg_resources.working_set:
-    print(f"Package: {dist.key}, Version: {dist.version}, Location: {dist.location}")
